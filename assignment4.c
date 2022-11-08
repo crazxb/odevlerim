4d6ea3f7c23abf146bc696925f54f520f9bf9c36
FURKAN EMİR KAYA <furkanemirkaya1905@gmail.com>
2022-11-08 23:01:44 +0300

    committed

---
+++ assignment4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int satir,i,j,bosluk;
+    printf("Satir sayisini giriniz:");
+    scanf("%d", &satir);
+    for(i=1;i<=satir;i++){
+        for(j=1;j<=satir-i;j++){
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+            printf("\n");
+        }
+    system("pause");
+    return 0;
+}
